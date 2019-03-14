5f48071ccea95289f8afb3d7860dacf1052ba555
Jaimin Patel <jaiminabpatel@ou.edu>
2019-03-14 11:43:37 -0500

    Foundations/Created a few classes

---
+++ src/BoardGame.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class BoardGame {
+	protected LinkedHashMap<String, GamePiece> playerPieces = new LinkedHashMap<>();
+	protected LinkedHashMap<String, Location> playerLocations = new LinkedHashMap<>();
+	
+	
+}
