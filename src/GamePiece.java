fa08eba39eb402dd3e5fbdbc90f5e731fab49dab
Jaimin Patel <jaiminabpatel@ou.edu>
2019-03-14 12:03:24 -0500

    Finished shape, color, location, started on gamePiece

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -1,4 +1,14 @@
 
 public enum GamePiece {
-
+	RED_RACER(red, racecar, 0), BLUE_RACER(blue, racecar, 2), MAGENTA_RACER(magenta, racecar, 1), RED_THIMBLE(red, thimble, 10), BLUE_BOOT(blue, boot, 5), GREEN_BOOT(green, boot, 8), YELLOW_BOOT(yellow, boot, 7);
+	
+	String color;
+	String shape;
+	int priority;
+	
+	private GamePiece(String color, String shape, int priority) {
+		this.color = color;
+		this.shape = shape;
+		this.priority = priority;
+	}
 }
---
+++ src/Shape.java
@@ -0,0 +1,9 @@
+
+public enum Shape {
+	
+	BOOT, RACECAR, THIMBLE;
+	
+	public String toString() {
+		return this.name().toString();
+	}
+}
